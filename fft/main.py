509353f5f3d105e3f0af3ca2326f7c21a3b47c00
jackzhou018 <jackzhou018@gmail.com>
2025-12-06 11:38:22 -0500

    .gitignore not working

--- fft/main.py
+++ fft/main.py
@@ -4,7 +4,7 @@ import utils.output as output
 
 
 if __name__ == "__main__":
-    x, y = parser.parse_csv("data_19700101_000403.csv")
+    x, y = parser.parse_csv("../data_19700101_000403.csv")
     Y = parser.fft(parser.window(parser.graph(x, y)))
     time = parser.time(1/2000, Y)
     parser.inverse_fft(time, Y)
